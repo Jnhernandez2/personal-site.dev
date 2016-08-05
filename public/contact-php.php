b8e02d14bfe71383e3fe5d6f33bed5211233e622
John Hernandez <johnhernandez@Johns-MacBook-Air.local>
2016-08-05 10:59:13 -0500

    I've begun the process of making my contact me page functional. I've changed it from an html file to a php file and have edited the links in all the other pages to link to the new php file page.

--- public/contact-php.php
+++ public/contact-php.php
@@ -1,22 +1,34 @@
+<?php
+var_dump($_GET);
+var_dump($_POST);
+
+function pageController() {
+
+
+
+}
+
+extract(pageController());
+
+?>
 <!DOCTYPE html>
-<html>
-	<head>
+<html lang="en">
+  	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="description" content="">
-    	<meta name="author" content="">
-    	<title>John Hernandez: Resume</title>
-    	<link rel="icon" href="../../favicon.ico">
+		<meta name="author" content="">
+		<title>Contact me</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 		<link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
 		<link href="/css/bootstrap-3.3.6/docs/examples/cover/cover.css" rel="stylesheet">
-		<link href="/css/custom-cover.css" rel="stylesheet">
-		
-	</head>
-	<body>
+		<link href="/css/bootstrap-3.3.6/docs/examples/signin/signin.css" rel="stylesheet">
+		<link href="/css/custom-contact.css" rel="stylesheet">
+   	</head>
+  	<body class="blacken">
 
-	    <div class="site-wrapper">
+  		<div class="site-wrapper">
 
 	      	<div class="site-wrapper-inner">
 
@@ -28,35 +40,31 @@
 	              			<nav>
 				                <ul class="nav masthead-nav">
 				                  <li><a href="/index.html">Home</a></li>
-				                  <li class="active"><a href="#">Projects</a></li>
-				                  <li><a href="/contact-php.php">Contact</a></li>
+				                  <li><a href="/projects.html">Projects</a></li>
+				                  <li class="active"><a href="/contact-php.html">Contact</a></li>
 				                </ul>
 	              			</nav>
 	            		</div>
 	          		</div>
 
-			        <div class "text-left">
-			        	<div>
-			        		<h2>Codeup Projects</h2>
-			        	</div>
-			        	<div>
-			        		<h3>Javascript Calculator</h3>
-			        		<!-- <a href=""> --><img src="/img/calculator-shot.png" alt="Javascript Calculator"><!-- </a> -->
-			        	</div>
-			        	<div>
-			        		<h3>Weather Map API</h3>
-			        		<!-- <a href=""> --><img src="/img/weather-app.png" alt="Weather Map API"><!-- </a> -->
-			        	</div>
-			        	<div>
-			        		<h3>Simon Says</h3>
-			        		<!-- <a href=""> --><img src="/img/clockwork-simon.png" alt="A Clockwork Simon"><!-- </a> -->
-			        	</div>
-
+			        <div class="inner cover">
+			            
 			        </div>
-
+						<form class="cover-heading form-signin">
+							<h2 class="form-signin-heading">Contact me</h2>
+							<label for="inputEmail" class="sr-only"></label>
+							<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+							<label for="extra-margin" class="sr-only"></label>
+							<input id="extra-margin" class="form-control" placeholder="Subject" required>
+							<textarea class="form-control" id="extra-margin" placeholder="Compose Message" required></textarea>
+							
+						</form>
+						<div class="form-signin">
+								<a href="mailto:jnhernandez2.0@gmail.com"><button class="btn btn-lg btn-primary btn-block" type="submit">Send</button></a>
+							</div>
 	          		<div class="mastfoot">
-			            <div class="inner">
-			            	<div class="row">
+		            	<div class="inner">
+		              		<div class="row">
 			            		<div class="col-md-6">
 			            			<div class="contact-icons">
 			            				<a href="https://github.com/Jnhernandez2/Codeup-Web-Exercises/tree/master/public"><img id="github-logo" src="/img/github-logo.png"></a>
@@ -68,7 +76,7 @@
 			            			</div>
 			            		</div>
 			            	</div>
-			            </div>
+		            	</div>
 		          	</div>
 
 	        	</div>
@@ -78,10 +86,26 @@
 	    </div>
 
 
+
+
+
+
+
+
+
+
+
+		  
+
+				
+
+				  
+
+				
+
+
 		<script src="../../assets/js/ie-emulation-modes-warning.js"></script>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-		<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-		<script src="/js/bootstrap.min.js"></script>
 		<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+		
 	</body>
 </html>
